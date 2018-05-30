fbff4ed9d7d5be6b3544688f68ceb37865c82caf
Administrator <yangningxiao123@163.com>
2018-05-30 21:10:22 +0800

    学习

--- MF/e13-classifier.py
+++ MF/e13-classifier.py
@@ -1,30 +1,34 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
 
 # 在 layer 中为 Weights, biases 设置变化图表
-def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
-    layer_name = 'layer%s' % n_layer
+def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
     with tf.name_scope('layer'):
         with tf.name_scope('weights'):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
-            tf.summary.histogram(layer_name + '/weights', Weights)  # tensorflow >= 0.12
         with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
-            tf.summary.histogram(layer_name + '/biases', biases)  # Tensorflow >= 0.12
         with tf.name_scope('Wx_plus_b'):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b, )
-
-        # tf.histogram_summary(layer_name+'/outputs',outputs)
-        tf.summary.histogram(layer_name + '/outputs', outputs)  # Tensorflow >= 0.12
         return outputs
+
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs})
+    correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys})
+    return result
 
 
 # 数据中包含55000张训练图片，每张图片的分辨率是28×28，所以我们的训练网络输入应该是28×28=784个像素数据。
@@ -33,3 +37,25 @@ xs = tf.placeholder(tf.float32, [None, 784])  # 28x28
 # 每张图片都表示一个数字，所以我们的输出是数字0到9，共10类。
 ys = tf.placeholder(tf.float32, [None, 10])
 
+# 调用add_layer搭建一个最简单的训练网络结构
+prediction = add_layer(xs, 784, 10, activation_function=tf.nn.softmax)
+
+# loss函数（即最优化目标函数）选用交叉熵函数。交叉熵用来衡量预测值和真实值的相似程度，如果完全相同，它们的交叉熵等于零
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))
+
+# train方法（最优化算法）采用梯度下降法
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+# 现在开始train，每次只取100张图片，免得数据太多训练太慢
+batch_xs, batch_ys = mnist.train.next_batch(100)
+sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+
+# 每训练50次输出一下预测精度
+for i in range(10000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    if i % 50 == 0:
+        print(compute_accuracy(mnist.test.images, mnist.test.labels))

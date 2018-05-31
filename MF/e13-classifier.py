98c142e1f2b0cd4f86a562253c3c0459a32a6e79
yangnx <yangningxiao123@163.com>
2018-05-31 15:58:30 +0800

    学习

--- MF/e13-classifier.py
+++ MF/e13-classifier.py
@@ -8,18 +8,14 @@ mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 # 在 layer 中为 Weights, biases 设置变化图表
 def add_layer(inputs, in_size, out_size, activation_function=None):
     # add one more layer and return the output of this layer
-    with tf.name_scope('layer'):
-        with tf.name_scope('weights'):
-            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
-        with tf.name_scope('biases'):
-            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
-        with tf.name_scope('Wx_plus_b'):
-            Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
-        if activation_function is None:
-            outputs = Wx_plus_b
-        else:
-            outputs = activation_function(Wx_plus_b, )
-        return outputs
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+    Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b, )
+    return outputs
 
 
 def compute_accuracy(v_xs, v_ys):
@@ -54,7 +50,7 @@ batch_xs, batch_ys = mnist.train.next_batch(100)
 sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
 
 # 每训练50次输出一下预测精度
-for i in range(10000):
+for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
     if i % 50 == 0:
